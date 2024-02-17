b22888183fd7018bd495d48d4fe0447d223ded46
Mara Caoile <mara.caoile@gmail.com>
2024-02-17 13:27:14 -0600

    loaded and seeded data

--- watchpartyapi/models/show.py
+++ watchpartyapi/models/show.py
@@ -4,7 +4,7 @@ from watchpartyapi.models import User, ShowGenre
 class Show(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     show_title = models.CharField(max_length=300)
-    show_description = models.CharField(max_length=20)
+    show_description = models.CharField(max_length=1000)
     show_poster = models.CharField(max_length=500)
     show_genre = models.ForeignKey(ShowGenre, on_delete=models.CASCADE)
     is_watching = models.BooleanField()
